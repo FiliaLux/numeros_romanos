e4ee38cc180e3a2bf79f9661956b0b4d021e35c7
Angie <rodriguezangela95@gmail.com>
2025-02-04 16:48:45 +0100

    Segundo planteamiento ampliando el rango de conversión de numeros

--- numeros_romanos.py
+++ numeros_romanos.py
@@ -16,6 +16,16 @@ def a_romanos(num:int) -> str:
         if value == num:
             roman = key
     return roman
+def b_romanos(num:int):
+    roman = ""
+    dictionary = list(numeros_romanos.items()) 
+    for key,value in dictionary[::-1]:
+        if num % value == 0:
+            roman = key * int(num/value)
+            break
+        else:
+            roman = a_romanos(num)
+    return roman
 
 
 assert a_romanos(1) == "I"
