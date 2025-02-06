a35a8520981f7a43c629cf597d055aa5329735b0
Angie <rodriguezangela95@gmail.com>
2025-02-06 17:06:16 +0100

    Funcion que transforma enteros en numeros romanos

--- numeros_romanos.py
+++ numeros_romanos.py
@@ -1,11 +1,17 @@
 result = 0
 numeros_romanos = {
     "I": 1,
+    "IV": 4,
     "V": 5,
+    "IX": 9,
     "X": 10,
+    "XL": 40,
     "L": 50,
+    "XC": 90,
     "C": 100,
+    "CD": 400,
     "D": 500,
+    "CM": 900,
     "M": 1000
 }
 
@@ -16,19 +22,18 @@ def a_romanos(num:int) -> str:
         if value == num:
             roman = key
     return roman
-def b_romanos(num:int):
+
+def int_a_romanos(num:int):
+    dictionary = sorted(numeros_romanos.items(), key = lambda x: x[1], reverse = True)
     roman = ""
-    dictionary = list(numeros_romanos.items()) 
-    for key,value in dictionary[::-1]:
-        if num % value == 0:
-            roman = key * int(num/value)
-            break
-        else:
-            roman = a_romanos(num)
+    for key, value in dictionary:
+        while num >= value:
+            roman += key
+            num -= value
     return roman
 
 
 assert a_romanos(1) == "I"
 assert a_romanos(6) != "V"
-
-
+assert int_a_romanos(456) == "CDLVI"
+assert int_a_romanos(93) != "LXXXXIII"
