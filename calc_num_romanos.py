67c89f48ce8ea02e01fb0f4da1d32170dc3ce27e
Angie <rodriguezangela95@gmail.com>
2025-02-13 21:39:26 +0100

    corrections

--- calc_num_romanos.py
+++ calc_num_romanos.py
@@ -1,20 +1,27 @@
 import numeros_romanos
+
 num_symbols = ["+","-","*","/","**","(",")","%","="]
+
 def romano_a_int(num:str):
+    
     number = 0
     prev_number = 0
+    
     for roman in reversed(num):
         if prev_number > numeros_romanos.numeros_romanos[roman]:
             number -= numeros_romanos.numeros_romanos[roman]
         else:
             number += numeros_romanos.numeros_romanos[roman]
         prev_number = numeros_romanos.numeros_romanos[roman]
+    
     return number 
 
 def number_input():
+    
     number = input("Enter number: ")
     symbol = input("Enter operation: ")
     operation = ""
+    
     while True:
         if number != "" and symbol in num_symbols:
             operation += number + " "
@@ -31,10 +38,12 @@ def number_input():
     return operation
 
 def calculator(operation:str):
+        
         num = ""
         nums = []
         operators = ""
         result = 0
+        
         for element in operation:
             if element in numeros_romanos.numeros_romanos:
                 num += element
@@ -42,6 +51,7 @@ def calculator(operation:str):
                 operators += element
                 nums.append(romano_a_int(num))
                 num = ""
+        
         for i,operator in enumerate(operators):
             if operator == "*":
                 if i == 1:
@@ -66,9 +76,7 @@ def calculator(operation:str):
                 if operators[i+1] in "*/":
                     pass
                 else:
-                    result += nums[i] - nums[i+1]
+                    result -= nums[i] - nums[i+1]
             elif operator == "=":
                 print(int(result))
                 print(numeros_romanos.int_a_romanos(result))
-
-    
